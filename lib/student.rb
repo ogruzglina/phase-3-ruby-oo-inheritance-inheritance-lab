ebd7bedfe22cf91923870fcdeb71aaa551464f76
Olha Gruzglina <olga.zhmura@gmail.com>
2022-02-26 12:10:28 -0500

    commit tree b4e0c7c01d0651fb6850686df1fd4dd56877b7bd

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge_name)
+        @knowledge << knowledge_name
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
